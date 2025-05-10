2c634daa397678b4ee3b2b9dcd04e6149a740420
Pricter <Pricter>
2025-05-10 10:34:52 +0530

    Platform layer

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,16 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
+#include <platform/platform.h>
+
 int main(void) {
+    platform_state state;
+    if(platform_startup(&state, "Nynx Engine Testbed", 100, 100, 1280, 720)) {
+        while(TRUE) {
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
+
     return 0;
 }
