57384ec9a3d996c036e66174704bdd50d80ff1f0
Pricter <Pricter>
2025-05-16 12:54:55 +0530

    Event subsystem

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -3,6 +3,7 @@
 #include "platform/platform.h"
 #include "game_types.h"
 #include "core/nmemory.h"
+#include "core/event.h"
 
 typedef struct application_state {
     game* game_inst;
@@ -30,6 +31,11 @@ b8 application_create(game* game_inst) {
 
     app_state.is_running = TRUE;
     app_state.is_suspended = FALSE;
+
+    if(!event_initialize()) {
+        NERROR("Event system failed initialization. Application cannot continue");
+        return FALSE;
+    }
 
     if(!platform_startup(
         &app_state.platform,
@@ -76,6 +82,8 @@ b8 application_run() {
     }
     
     app_state.is_running = FALSE;
+
+    event_shutdown();
     platform_shutdown(&app_state.platform);
     
     return TRUE;
